5bb28d3084ba0c764fd9b5d221abdd5648eb26ff
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-09-25 12:48:11 -0400

    Created JWT helper module, added the token generation and verification in middlewear and then used it in the controller

--- Backend Practise/src/routes/routes.js
+++ Backend Practise/src/routes/routes.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const userAuthentication = require("../Middlewear/userMiddlewear");
 
-router.get(
-  "/middlewear",
-  userAuthentication.authenticateUser,
-  userController.authenticatedUser
-);
+router.get("/middlewear",userAuthentication.authenticateUser,userController.authenticatedUser);
 router.get("/getTheUser", userController.getUser);
 router.get("/getSingleUser/:id", userController.getSingleUser);
 router.delete("/deleteUser/:id", userController.deleteUser);
